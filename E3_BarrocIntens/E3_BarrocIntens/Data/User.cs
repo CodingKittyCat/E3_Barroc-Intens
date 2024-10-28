3df02f1b685e9dbe1f1af90098aa2a8cf1cd0a1d
ThijsHeuvel <d294396@edu.curio.nl>
2024-10-28 11:44:00 +0100

    Created user class

---
+++ E3_BarrocIntens/E3_BarrocIntens/Data/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E3_BarrocIntens.Data
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
