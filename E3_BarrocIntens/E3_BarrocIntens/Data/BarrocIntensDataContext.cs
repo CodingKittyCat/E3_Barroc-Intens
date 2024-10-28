d4453db4e5cd0105c657afa09952b291f237894e
ThijsHeuvel <d294396@edu.curio.nl>
2024-10-28 11:43:51 +0100

    Added database to save users and other data in

---
+++ E3_BarrocIntens/E3_BarrocIntens/Data/BarrocIntensDataContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace E3_BarrocIntens.Data
+{
+    class BarrocIntensDataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "server=localhost;" +
+                "port=3306;" +
+                "user=root;" +
+                "password=;" +
+                "database=barrocintens",
+                ServerVersion.Parse("10.4.17-mariadb")
+            );
+        }
+    }
+}
