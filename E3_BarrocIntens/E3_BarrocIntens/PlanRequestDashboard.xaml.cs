45c58a6643a2d9557039e1596de2599e8c4142e4
ThijsHeuvel <d294396@edu.curio.nl>
2024-11-20 11:14:28 +0100

    Added back button to PlanRequestDashboard

--- E3_BarrocIntens/E3_BarrocIntens/PlanRequestDashboard.xaml.cs
+++ E3_BarrocIntens/E3_BarrocIntens/PlanRequestDashboard.xaml.cs
@@ -59,5 +59,10 @@ namespace E3_BarrocIntens
                 this.Frame.Navigate(typeof(ViewDateDashboard), requestDate);
             }
         }
+
+        private void backBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(ViewDateDashboard), requestDate);
+        }
     }
 }
